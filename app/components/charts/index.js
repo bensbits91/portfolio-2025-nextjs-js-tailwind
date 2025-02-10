8d28f7d6a3dce596ae230e19ce5335a2602818f1
bensbits91 <benbpnw@gmail.com>
2025-02-09 16:40:04 -0800

    remove unused LineChart component

--- app/components/charts/index.js
+++ app/components/charts/index.js
@@ -1,4 +1,3 @@
-export { default as LineChart } from './LineChart';
 export { default as BarChart } from './BarChart';
 export { default as AreaChart } from './AreaChart';
 export { default as RadarChart } from './RadarChart';
