3fd3e96adc7d57238f25d15d57d3821a78591902
bensbits91 <benbpnw@gmail.com>
2025-03-04 09:55:38 -0800

    fix skill font size on desktop

--- app/components/skills/SkillItem.js
+++ app/components/skills/SkillItem.js
@@ -4,7 +4,7 @@ import { generateStars } from '@/app/utils';
 const SkillItem = ({ item }) => {
    const { name, cloudinary, rating, years } = item;
    return (
-      <li className='border-b border-bb-gray-800 py-2'>
+      <li className="border-b border-bb-gray-800 py-2">
          <div className="grid h-10 grid-cols-[1fr,3fr,3fr,3fr] items-center gap-4">
             {cloudinary && (
                <div className="mr-2 flex h-4 w-4">
@@ -16,8 +16,10 @@ const SkillItem = ({ item }) => {
                   />
                </div>
             )}
-            <div className="text-[0.7rem]">{name}</div>
-            <div className="min-w-24 text-[0.7rem] whitespace-nowrap">~ {years} years</div>
+            <div className="text-[0.7rem] md:text-sm">{name}</div>
+            <div className="min-w-24 whitespace-nowrap text-[0.7rem] md:text-sm">
+               ~ {years} years
+            </div>
             <div className="flex h-4">{generateStars(rating)}</div>
          </div>
       </li>
