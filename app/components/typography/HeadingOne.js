e73980c40bba057b3919a2c2c07effb5891a6609
bensbits91 <benbpnw@gmail.com>
2025-04-06 13:43:13 -0700

    improve mobile h1

--- app/components/typography/HeadingOne.js
+++ app/components/typography/HeadingOne.js
@@ -1,8 +1,8 @@
 const HeadingOne = ({ children, textCenter = false }) => (
    <h1
-      className={`mb-8 font-roboto-mono ${
+      className={`mb-2 md:mb-8 font-roboto-mono ${
          textCenter ? 'md:text-center' : ''
-      } text-5xl/[50px] md:text-6xl/[50px] tracking-tight`}>
+      } text-4xl/[50px] md:text-6xl/[50px] tracking-tight`}>
       {children}
    </h1>
 );
