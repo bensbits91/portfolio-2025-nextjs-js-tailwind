5b92aab77a3de2e69911bfb6b74e47fe4a7d72ad
bensbits91 <benbpnw@gmail.com>
2025-04-25 15:30:50 -0700

    cleanup

--- components/typography/index.js
+++ components/typography/index.js
@@ -1,6 +1,5 @@
 export { default as HeadingOne } from './HeadingOne';
 export { default as HeadingTwo } from './HeadingTwo';
 export { default as HeadingThree } from './HeadingThree';
-export { default as WhisperText } from './WhisperText';
 export { default as Text } from './Text';
 export { default as UL } from './UL';
